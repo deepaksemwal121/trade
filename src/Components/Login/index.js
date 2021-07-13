ea820080010fcea3429f70a058ed7319f65e20a1
Akhilbisht12 <akhilbisht83@gmail.com>
2021-07-13 23:41:02 +0530

    material ui added | login page in progess

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import LogImg from '../../Assets/Login.svg'
 
 const Login = () => {
-  return <div>login</div>;
+  return(
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid style={styles.loginTextBox} item xs={12} sm={6}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            Login
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Welcome back to your community of amazing people
+          </Typography>
+        </Grid>
+        <Grid style={{backgroundColor : '', padding : '1rem'}} item xs={12} sm={6}>
+          <img style={{width : '80%'}} alt='login' src={LogImg}/>
+        </Grid>
+      </Grid>
+    </Container>
+    );
 };
+
+var styles = {
+  loginTextBox : {
+    padding : '1rem',
+    backgroundColor : 'blue',
+    color : 'white',
+    borderRadius : '10px 0 0 10px'
+  }
+}
 
 export default Login;
